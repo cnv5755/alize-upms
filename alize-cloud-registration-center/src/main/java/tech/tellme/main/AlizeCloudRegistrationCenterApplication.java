642a8e2dd7b8a56fa8d2b4141e6baa7f23c8cfd7
yb <ediii@qq.com>
2019-02-13 10:12:59 +0800

    update alize-cloud-registration-center server

--- alize-cloud-registration-center/src/main/java/tech/tellme/main/AlizeCloudRegistrationCenterApplication.java
+++ alize-cloud-registration-center/src/main/java/tech/tellme/main/AlizeCloudRegistrationCenterApplication.java
@@ -2,6 +2,8 @@ package tech.tellme.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Spring Cloud Eureka 注册中心
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019年2月12日 17:23:17
  */
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
 public class AlizeCloudRegistrationCenterApplication {
 
     public static void main(String[] args) {
---
+++ alize-cloud-registration-center/src/main/java/tech/tellme/main/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package tech.tellme.main;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author yb
+ * @date 2019年2月13日 10:08:36
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
